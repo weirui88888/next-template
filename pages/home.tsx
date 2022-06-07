32fe656c3a0041fcb7480a4d635af82329a431e5
weirui88888 <ruiwei88888@163.com>
2022-06-07 11:41:21 +0800

    feat: :sparkles:add classnames

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import styles from '../styles/demo.module.scss'
-
+import cn from 'classnames'
 function Home() {
   return (
     <div className={styles.div}>
-      <span>hello</span>
+      <span className={styles.span}>hello</span>
+      <p
+        className={cn({
+          [styles['p-red']]: true,
+          btn: true
+        })}
+      >
+        你好
+      </p>
     </div>
   )
 }
